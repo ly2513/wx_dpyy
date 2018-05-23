d4ee1774814bbdff892b067246012dedc470b5ff
ly2513 <626375290@qq.com>
2018-05-24 00:35:24 +0800

    update

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,7 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var dev = false;
+var dev = true;
 // 后端地址
 app.globalData.requestUrl = (dev===true) ? 'http://127.0.0.1:1025' : 'http://192.168.1.101:4000';
 // openID
@@ -15,11 +15,14 @@ Page({
     })
   },
   onLoad: function () {
+    
+  },
+  userLogin:function(){
     wx.login({
       success: function (res) {
         console.log(res);
         if (res.code) {
-           //发起网络请求
+          //发起网络请求
           wx.request({
             url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wxdf7ab6b5266fa384&secret=7b6bcdd8eaf5292cbfcf79eb71b63379&js_code=' + res.code + '&grant_type=authorization_code',
             data: {},
@@ -27,6 +30,7 @@ Page({
               'content-type': 'application/json'
             },
             success: function (res) {
+              
               app.globalData.openId = res.data.openid //返回openid
               // 获取用户信息
               wx.getUserInfo({
@@ -46,8 +50,8 @@ Page({
                       success: function (res) {
                         console.log(res);
                         // console.log('请求成功！')
-                        if (res.data.code === 0){
-                        // 登录成功后跳转到首页
+                        if (res.data.code === 0) {
+                          // 登录成功后跳转到首页
                           wx.switchTab({
                             url: '../menu/menu',
                             success: function (res) {
@@ -58,7 +62,7 @@ Page({
                               console.log('跳转失败');
                             }
                           })
-                        }else{
+                        } else {
                           console.log(res.data.msg)
                         }
                       },
