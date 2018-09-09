b94c33f95649b1e84547d6031bc2fae37cce5b55
ly2513 <626375290@qq.com>
2018-09-09 21:57:46 +0800

    添加图片

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    // wx.login({
-    //   success: res => {
-    //       console.log(res);
-
-    //       // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
     // 获取用户信息
     wx.getSetting({
       success: res => {
