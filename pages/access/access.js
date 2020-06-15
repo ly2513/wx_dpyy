438f4be859b26ce40e4e47ff74cdcb58c1722e27
zhangbaogang123 <972010422@qq.com>
2020-06-16 01:31:39 +0800

    存储用户信息，同步方法改成异步

--- pages/access/access.js
+++ pages/access/access.js
@@ -15,7 +15,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -26,56 +26,70 @@ Page({
     });
   },
   agreeGetUser: function (e) { //获取用户信息新接口
-    wx.setStorageSync('userInfo', e.detail.userInfo);
+    // wx.setStorageSync('userInfo', e.detail.userInfo);
     let that = this;
-    wx.showModal({
-      title: '系统提示',
-      content: '为确保用户的文印数据安全和服务，请您设置手机号码，以此作平台唯一的安全身份标识。',
-      showCancel: true,
-      success: function (resbtn) {
-        that.setData({
-          hidden: false
-        });
-        if (resbtn.confirm) {
-          //设置用户信息本地存储
-          // 跳转登录页
-          wx.navigateTo({
-            url: '../login/login'
-          });
-          that.setData({
-            hidden: true
-          });
-        }
+
+    wx.setStorage({
+      data: e.detail.userInfo,
+      key: 'userInfo',
+      success: function () {
+        wx.showModal({
+          title: '系统提示',
+          content: '为确保用户的文印数据安全和服务，请您设置手机号码，以此作平台唯一的安全身份标识。',
+          showCancel: true,
+          success: function (resbtn) {
+            that.setData({
+              hidden: false
+            });
+            if (resbtn.confirm) {
+              //设置用户信息本地存储
+              // 跳转登录页
+              wx.navigateTo({
+                url: '../login/login'
+              });
+              that.setData({
+                hidden: true
+              });
+            }
+          }
+        })
+      },
+      fail: function () {
+        wx.showModal({
+          title: '温馨提示',
+          content: '用户信息存储失败',
+        })
       }
     })
+
   },
-  noticeUserMsg: function (e){
+  noticeUserMsg: function (e) {
     wx.showModal({
       title: '温馨提示',
       content: '为确保用户的文印数据安全和服务，请您设置手机号码，以此作平台唯一的安全身份标识。',
       success: function (res) {
         console.log(res);
         // 取消
-        if (res.cancel){
+        if (res.cancel) {
           // 跳转到菜单
           wx.switchTab({
             url: '../menu/menu',
           })
         }
         // 确定
-        if(res.confirm){
+        if (res.confirm) {
           // 跳转到注册页面
           wx.navigateTo({
             url: '../register/register',
           })
         }
       },
-      fail: function (rs){
+      fail: function (rs) {
         wx.showModal({
           title: '提示',
           content: '网络异常，请稍后继续操作。',
           success: function (res) {
-            
+
           },
           fail: function (rs) {
 
